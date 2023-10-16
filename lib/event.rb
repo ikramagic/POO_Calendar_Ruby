14a9d8c5427dbf4da7c54ee3c495a09fcc502781
ikramiste <ikrame.saadi@gmail.com>
2023-10-16 14:56:32 +0200

    start date + duration done

--- lib/event.rb
+++ lib/event.rb
@@ -1,2 +1,9 @@
-require 'bundler'
-Bundler.require
+class Event
+    attr_accessor :start_date, :duration, :title, :attendees
+
+    def initialize(chosen_time, duration_time)
+        require 'time'
+        @start_date = Time.parse(chosen_time) #parse permet de convertir le string chosen_time en objet Time utilisable comme tel
+        @duration = duration_time
+    end
+end
