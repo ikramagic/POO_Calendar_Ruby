ed27f19dd76bb378cb1e894ebcbbd3d45838b48f
ikramiste <ikrame.saadi@gmail.com>
2023-10-16 15:09:26 +0200

    instances set and functional

--- lib/event.rb
+++ lib/event.rb
@@ -1,9 +1,11 @@
 class Event
     attr_accessor :start_date, :duration, :title, :attendees
 
-    def initialize(chosen_time, duration_time)
+    def initialize(chosen_time, duration_time, given_name, peeps)
         require 'time'
         @start_date = Time.parse(chosen_time) #parse permet de convertir le string chosen_time en objet Time utilisable comme tel
         @duration = duration_time
+        @title = given_name
+        @attendees = [peeps]
     end
 end
