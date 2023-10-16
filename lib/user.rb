0146bfa3f689e21583b1bc0db5a340d9fdfa0665
ikramiste <ikrame.saadi@gmail.com>
2023-10-16 13:19:35 +0200

    age and email assigned to user instance

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,14 @@
 require 'bundler'
 Bundler.require
 
+class User
+    attr_accessor :email, :age
+
+    def initialize(email_def, age_def)
+        @email = email_def
+        @age = age_def
+      end
+end
+
+binding.pry
+puts "ended test"
