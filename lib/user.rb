86f3eedf2cd90e44939db3d14e67a3281d15ba55
ikramiste <ikrame.saadi@gmail.com>
2023-10-16 14:25:35 +0200

    users completed and functional

--- lib/user.rb
+++ lib/user.rb
@@ -1,14 +1,14 @@
-require 'bundler'
-Bundler.require
-
 class User
     attr_accessor :email, :age
+    @@all_users = []
 
     def initialize(email_def, age_def)
         @email = email_def
         @age = age_def
-      end
-end
+        @@all_users << self
+    end
 
-binding.pry
-puts "ended test"
+    def self.all #self ferme l'accès aux données en dehors de la méthode de classe, self = sera utilisée uniquement dans cette méthode
+        return @@all_users
+    end
+end
